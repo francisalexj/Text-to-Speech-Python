f043aa8c302ae1a905442954dcb3309a04a63988
Alex <alex@ventunotech.com>
2018-02-09 15:52:22 +0530

    Google token file added.

--- conversion/views.py
+++ conversion/views.py
@@ -1,9 +1,17 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .lib.gtts_token import Token
+
 def index(request):
-	template = loader.get_template('index.html')
+	conversion_text = request.POST.get('conversion_text', '')
+	gts_token = Token()
+	gen_token = gts_token.calculate_token(conversion_text)
+
 	content = {
-		'foo': 'Alter bar'
+		'conversion_text': conversion_text,
+		'gen_token' : gen_token
 	}
+	template = loader.get_template('index.html')
 	return HttpResponse(template.render(content, request))
