856bac43def95ac094710831f5408736b756d982
Alex <alex@ventunotech.com>
2018-02-14 17:44:47 +0530

    Translation page added.

--- conversion/views.py
+++ conversion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,13 +6,31 @@ from .lib.gtts import gTTS
 from .models import ConversionModel
 
 def index(request):
+	conversion_text = ''
+	conversion_lang = 'en'
+
+	language_list = gTTS.LANGUAGES.items()
+
+	content = {
+		'conversion_text': conversion_text,
+		'language_list' : language_list,
+		'selected_lang' : conversion_lang,
+	}
+	template = loader.get_template('index.html')
+	return HttpResponse(template.render(content, request))
+
+def translate(request):
+	if request.method != "POST":
+		return redirect('/')
+
 	conversion_text = request.POST.get('conversion_text', '')
 	conversion_lang = request.POST.get('conversion_lang', 'en')
 
 	cvm = ConversionModel()
 
-	language_list = gTTS.LANGUAGES.items()
-	selected_lang = conversion_lang
+	language_list_master = gTTS.LANGUAGES
+	language_list = language_list_master.items()
+	selected_lang = (language_list_master[conversion_lang] if conversion_lang in language_list_master else "")
 
 	audio_save_path = 'static/files/' + cvm.id_generator() + ".mp3"
 	conversion_status = False
@@ -29,5 +47,5 @@ def index(request):
 		'language_list' : language_list,
 		'selected_lang' : selected_lang,
 	}
-	template = loader.get_template('index.html')
+	template = loader.get_template('translate.html')
 	return HttpResponse(template.render(content, request))
